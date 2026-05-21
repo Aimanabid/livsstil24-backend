da630fdc03a199c6a7d1429cb6f490365c468d48
root <root@ip-172-31-47-100.eu-north-1.compute.internal>
2026-05-21 08:51:54 +0000

    server side push

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,15 @@
-import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME ||"mg",
+  process.env.DB_USER || "mg",
+  process.env.DB_PASSWORD || "mg110!",
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: "mysql",
     logging: false,
     define: {
       underscored: true,
